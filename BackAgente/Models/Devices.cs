1926035097c29e3b5c15f5a86be100709ce9853e
JulioM0 <juliomedina30543@gmail.com>
2025-03-04 12:01:47 -0600

    Primer commit

---
+++ BackAgente/Models/Devices.cs
@@ -0,0 +1,23 @@
+﻿namespace BackAgente.Models
+{
+    public class Devices
+    {
+        public int id { get; set; }
+        public int parentDeviceId { get; set; }
+        public int organizationId { get; set; }
+        public int locationId { get; set; }
+        public string nodeClass { get; set; }
+        public int nodeRoleId { get; set; }
+        public int rolePolicyId { get; set; }
+        public int policyId { get; set; }
+        public string approvalStatus { get; set; }
+        public bool offline { get; set; }
+        public string displayName { get; set; }
+        public string systemName { get; set; }
+        public string dnsName { get; set; }
+        public string netbiosName { get; set; }
+        public int created { get; set; }
+        public int lastContact { get; set; }
+        public int lastUpdate { get; set; }
+    }
+}
